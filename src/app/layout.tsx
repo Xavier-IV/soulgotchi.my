1e576e945db816b0fdb1ba73fe1ef2bc91c681ab
Zafranudin Zafrin <mohd.zafranudin@gmail.com>
2025-03-03 23:31:41 +0100

    feat: added analytic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { PostHogProvider } from "./provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -45,7 +46,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground`}
       >
-        {children}
+        <PostHogProvider>
+          {children}
+        </PostHogProvider>
         <script
           dangerouslySetInnerHTML={{
             __html: `
